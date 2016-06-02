7552683a3d78b63764b17f7abd0d78c5f08df81d
courtneysfoster <fostecou@oregonstate.edu>
2016-06-01 19:58:43 -0700

    Update

--- exerciseApp.js
+++ exerciseApp.js
@@ -102,7 +102,7 @@ app.get("/make-table",function(req,res,next){
 app.get('/insert',function(req,res,next){
   console.log("we got here");
   var context = {};
-  pool.query("INSERT INTO workouts (`exercise`, `reps`) VALUES (?)", [req.query.exercise], [req.query.reps] function(err, result){
+  pool.query("INSERT INTO workouts (`exercise`, `reps`) VALUES (?)", [req.query.exercise, req.query.reps] function(err, result){
     if(err){
       next(err);
       return;
