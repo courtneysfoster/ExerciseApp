b9a2edae0f5403ed0ca36171f52336d9e075b104
courtneysfoster <fostecou@oregonstate.edu>
2016-06-01 17:02:41 -0700

    Update201606011702

--- public/js/home.js
+++ public/js/home.js
@@ -3,9 +3,11 @@
 var submitURL = "http://54.213.219.47:3000/formSubmit";
 /* var submitURL = "http://localhost:3000/formSubmit"; */
 
+/*
 document.getElementById("btnSubmit").addEventListener("click", function(){	
 	var inputForm = document.getElementById("frmInput");
 	var req = new XMLHttpRequest();
+	var inputParams = [];
 	for(var i=0, iLen=inputForm.length; i<iLen; i++){
 		inputParams.push({"name":inputForm[i].name, "value":inputForm[i].value}); 
 	}
@@ -20,5 +22,6 @@ document.getElementById("btnSubmit").addEventListener("click", function(){
 function init(){
 	
 }
+*/
 
 
