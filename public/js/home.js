a24df9ffed946c6cc2ba122f11226393f5e3f4e4
courtneysfoster <fostecou@oregonstate.edu>
2016-06-02 16:50:07 -0700

    d

--- public/js/home.js
+++ public/js/home.js
@@ -30,11 +30,14 @@ function buildTable(response){
 	var tr;
 	for(var i=0, iLen=response.length; i<iLen; i++){
 		tr = document.createElement("tr");
+		console.log(response[i])
+		/*
 		var exerciseData = response[i];
 		for(var j=0; i<colNames.length; j++){
 			td.textContent = exerciseData[colNames[j]].value;
 			tr.appendChild(td);	
 		}
+		*/
 	}
 	document.getElementById("outputArea").appendChild(table);
 }
