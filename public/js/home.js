6558ec00d3dab016cbdcd266e845cedf5eff2e3e
courtneysfoster <fostecou@oregonstate.edu>
2016-06-02 14:54:23 -0700

    f

--- public/js/home.js
+++ public/js/home.js
@@ -54,6 +54,7 @@ document.getElementById("btnNew").addEventListener("click", function(event){
 	req.setRequestHeader("Content-Type", "application/json");
 	req.addEventListener("load", function(){
 		if (req.status>=200 && req.status<400){
+			console.log(req.response);
 			var response = JSON.parse(req.response);
 			console.log("req.status= " + req.status);
 			buildTable(response);
