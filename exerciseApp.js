99717ed369cc7dda4c0bbb634b65572922210f67
courtneysfoster <fostecou@oregonstate.edu>
2016-06-01 21:35:04 -0700

    Update

--- exerciseApp.js
+++ exerciseApp.js
@@ -40,14 +40,13 @@ app.get("/", function(req,res,next){
 			var dtDate = (new Date(Date.now())).toLocaleDateString('en-US');
 			context.date = (dtDate.getMonth() + 1) + "-" + dtDate.getDate() + "-" + dtDate.getFullYear();
 			*/
-			res.render("home", context);
+			res.send(context);
 		}
 	});
 });
 
 app.post("/formSubmit", function(req,res,next){
 	var context={};
-	console.log(req.body.exercise)
 	pool.query("insert into workouts" + 
 			  "(`exercise`, `reps`, `weight`, `date`, `lbs`)" +
 			  "values (?, ?, ?, ?, ?)"
