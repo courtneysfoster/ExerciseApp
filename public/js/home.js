f03bc25206a268c4b2667cdbbc1ddbabb112d8ff
courtneysfoster <fostecou@oregonstate.edu>
2016-06-02 00:40:38 -0700

    f

--- public/js/home.js
+++ public/js/home.js
@@ -63,30 +63,7 @@ function afterPageLoad(){
 	table.style.borderCollapse = "collapse";
 
 
-	var td;
-	for (var i=0; i<4; i++){
-		for (var j=0; j<4; j++){
-			td = document.createElement("td");
-			td.id = ("td"+j)+i;
-			switch (i){
-				// 0 = header row
-				case 0:
-					var nr = document.createElement("th");
-					table.appendChild(nr);
-					td.textContent = responseText.exercise;
-					break;
-				// else table data
-				default:
-					if (j==0){
-						var nr = document.createElement("tr");
-						nr.style.textAlign = "center";
-						table.appendChild(nr);
-					}
-					td.textContent = (j+1) + ", " + (i);		
-			}
-			nr.appendChild(td);
-		}
-	}
+
 	document.getElementById("outputArea").appendChild(table);
 }
 
