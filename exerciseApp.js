ff1f912727411daaa115a1cdeee97e846a597584
courtneysfoster <fostecou@oregonstate.edu>
2016-06-03 08:21:23 -0700

    WorkingOnDelete

--- exerciseApp.js
+++ exerciseApp.js
@@ -73,7 +73,7 @@ app.post("/insert", function(req,res,next){
 
 /* Delete Query */
 app.post("/delete", function(req,res,next){
-	console.log("did we make it here?");
+	console.log(req);
 	var context={};
 	pool.query("delete from workouts where id=?", [req.body.id], function(err, results){
 		if (err){
