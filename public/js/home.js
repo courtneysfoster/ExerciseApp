d6408ace9316262af7fe9a9e7da2a6a087319797
courtneysfoster <fostecou@oregonstate.edu>
2016-06-02 18:17:15 -0700

    d

--- public/js/home.js
+++ public/js/home.js
@@ -32,7 +32,7 @@ function buildTable(response){
 		tr = document.createElement("tr");
 		item = response[i];
 		for(var key in item){
-			console.log(item[key]);
+			console.log("Item keyVal for item "+i+" = " + item[key]);
 			if (item[key]=="lbs"){
 				if(item[key].value==1){
 					td.textContent = "Lbs";
