d3ab8e682814b2997fc3ef5e56d7c4c1c8261495
Courtney <fostecou@oregonstate.edu>
2017-11-19 18:10:01 -0800

    Update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,5 @@
 /*Exercise Tracker home page javascript*/
-var ip = "http://54.213.219.47:3000/";
+var ip = "http://52.23.211.180:6645/";
 
 document.addEventListener("DOMContentLoaded", afterPageLoad);
 
